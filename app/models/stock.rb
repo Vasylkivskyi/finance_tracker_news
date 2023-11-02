8f49aa42902d049e4be0a21a5dd07cb6cd158296
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2023-11-03 00:29:38 +0200

    fix issue with js

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,11 +5,11 @@ class Stock < ApplicationRecord
   validates :name, :ticker_symbol, presence: true
 
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:access_key],
-      endpoint: 'https://cloud.iexapis.com/v1'
-    )
     begin
+      client = IEX::Api::Client.new(
+        publishable_token: Rails.application.credentials.iex_client[:access_key],
+        endpoint: 'https://cloud.iexapis.com/v1'
+      )
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
