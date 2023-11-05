54e3218c5bebec8b00c4aad5a8195e69dd9fe60e
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2023-11-05 22:47:31 +0200

    add stocks to portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,21 +5,30 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker_symbol, presence: true
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
+    uppercased_ticker_symbol = ticker_symbol.upcase
     begin
-      stock = get_stock_data(ticker_symbol)
+      stock = get_stock_data(uppercased_ticker_symbol)
       return nil if stock.nil?
-      new(ticker: ticker_symbol, name: stock[:name], last_price: stock[:price])
+      pp "works"
+      new(ticker: uppercased_ticker_symbol, name: stock[:name], last_price: stock[:price])
     rescue => exception
       return nil
     end
   end
 
+  def self.check_db(ticker_symbol)
+    self.where(ticker: ticker_symbol).first
+  end
+
   private
   def self.get_stock_data(ticker)
-    date = Time.now.strftime("%Y-%d-%m")
+    time = Date.today
+    time = (time - 2) # checks with 2 days delay because free api plan
+    date = time.strftime("%Y-%m-%d")
+    pp "https://api.polygon.io/v2/aggs/ticker/#{ticker}/range/1/day/#{date}/#{date}?apiKey=#{Rails.application.credentials.poligonio[:access_key]}"
     response = RestClient.get("https://api.polygon.io/v2/aggs/ticker/#{ticker}/range/1/day/#{date}/#{date}?apiKey=#{Rails.application.credentials.poligonio[:access_key]}", {accept: :json})
     result = JSON.parse(response.body)
     if result["resultsCount"] == 0
